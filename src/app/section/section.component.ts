74abba2102a92d881e31081930597cfb461f0ed8
ismael <ismaelreyplata@gmail.com>
2024-03-13 12:02:20 +0100

    dos filters que incluyen pero no es que solo sean

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-section',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './section.component.html',
   styleUrl: './section.component.scss'
 })
@@ -12,9 +13,28 @@ export class SectionComponent {
 
 
   films: any;
+  token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTU2N2I3YjE4NDk0MzYzYTdhMjMwZGE4N2MzMjcxYSIsInN1YiI6IjY1ZjA3MThlMTdiNWVmMDE4NWI4Y2MzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6evinFLVwzkbSrxslCziL5_G-wL4F-rzwBCXWxhOADM";
+  headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`,
+      'Accept': 'application/json' // Este sería el tipo de dato que aceptas, puede ser application/json u otro según lo que necesites
+  });
+
+
+
+
 
   Api: string = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
 
+  ApiNowPlaying: string = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}";
+
+  
+  include_adult: boolean = false;
+  ApiAdult: string = `https://api.themoviedb.org/3/discover/movie?include_adult=${this.include_adult}&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}`;
+
+  include_video: boolean = false;
+  ApiVideo: string = `https://api.themoviedb.org/3/discover/movie?include_adult=${this.include_adult}&include_video=${this.include_video}&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}`;
+  
+  ApiAllFilter: string = `https://api.themoviedb.org/3/discover/movie?include_adult=${this.include_adult}&include_video=${this.include_video}&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}`;
   constructor(private httpClient: HttpClient){}
 
   ngOnInit(): void {
@@ -27,19 +47,51 @@ export class SectionComponent {
     // });
 
 
-    const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTU2N2I3YjE4NDk0MzYzYTdhMjMwZGE4N2MzMjcxYSIsInN1YiI6IjY1ZjA3MThlMTdiNWVmMDE4NWI4Y2MzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6evinFLVwzkbSrxslCziL5_G-wL4F-rzwBCXWxhOADM";
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-      'Accept': 'application/json' // Este sería el tipo de dato que aceptas, puede ser application/json u otro según lo que necesites
-    });
+    // const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTU2N2I3YjE4NDk0MzYzYTdhMjMwZGE4N2MzMjcxYSIsInN1YiI6IjY1ZjA3MThlMTdiNWVmMDE4NWI4Y2MzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6evinFLVwzkbSrxslCziL5_G-wL4F-rzwBCXWxhOADM";
+    // const headers = new HttpHeaders({
+    //   'Authorization': `Bearer ${token}`,
+    //   'Accept': 'application/json' // Este sería el tipo de dato que aceptas, puede ser application/json u otro según lo que necesites
+    // });
 
-    this.httpClient.get(this.Api, {headers})
+    const headers = this.headers;
+
+    this.httpClient.get(this.ApiNowPlaying, {headers})
     .subscribe((film:any)=> {
       console.log(film);
       this.films = film.results;
       console.log(this.films);
     });
     
+  }
+
+  filterAdult(){
+
+    this.include_adult = !this.include_adult;
+    console.log("filtro adulto:"+this.include_adult);
+    console.log("filtro video:"+this.include_video);
+    this.ApiAllFilter = `https://api.themoviedb.org/3/discover/movie?include_adult=${this.include_adult}&include_video=${this.include_video}&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}`;
+    const headers = this.headers;
+    this.httpClient.get(this.ApiAllFilter, {headers})
+    .subscribe((film:any)=> {
+      console.log(film);
+      this.films = film.results;
+      console.log(this.films);
+    });
+  }
+
+  filterVideo(){
+    this.include_video = !this.include_video;
+    console.log("filtro adulto:"+this.include_adult);
+    console.log("filtro video:"+this.include_video);
+    this.ApiAllFilter = `https://api.themoviedb.org/3/discover/movie?include_adult=${this.include_adult}&include_video=${this.include_video}&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}`;
+    const headers = this.headers;
+
+    this.httpClient.get(this.ApiAllFilter, {headers})
+    .subscribe((film:any)=> {
+      console.log(film);
+      this.films = film.results;
+      console.log(this.films);
+    });
   }
 
 }
